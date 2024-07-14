460be7d1283ec9365f04fbc1a0e1a57d5db3fd17
Joao Alfredo Cardoso Lamy <tntlamy@gmail.com>
2024-07-13 20:05:55 -0500

    cen

--- cm_cc/main.py
+++ cm_cc/main.py
@@ -1,8 +1,8 @@
 import os
 
-network = "orkut"
+network = "cen"
 
-itr = "100"
+itr = "1000"
 
 terminal_command = f"""
     cd ..
@@ -23,10 +23,30 @@ terminal_command = f"""
 
 os.system(terminal_command)
 
-# terminal_command = '''
-#     cd ..
-#     cd cm_pipeline
-#     python3 -m hm01.cm -i ../cm_cc/data/test/network.tsv -e ../cm_cc/data/test/clustering.tsv -o ../cm_cc/output/test/output.tsv -c nop --threshold 0.1 --nprocs 4 --quiet
-# '''
+terminal_command = f"""
+    cd ..
+    cd cm_pipeline
+    Rscript ./scripts/analysis.R ../cm_cc/data/{network}/edge_list_iids.tsv   ../cm_cc/data/{network}/{network}_output_itr_{itr}_clustering.tsv   ../cm_cc/output/{network}/{network}_output_itr_{itr}_cc.tsv
+"""
 
-# os.system(terminal_command)
+
+os.system(terminal_command)
+
+
+terminal_command = f"""
+    cd ..
+    cd cm_pipeline
+    Rscript ./scripts/analysis.R ../cm_cc/data/{network}/edge_list_iids.tsv   ../cm_cc/output/{network}/stats/{network}_output_itr_{itr}_cc_stats.tsv   ../cm_cc/output/{network}/{network}_output_itr_{itr}_cc.tsv
+"""
+
+
+os.system(terminal_command)
+
+terminal_command = f"""
+    cd ..
+    cd cm_pipeline
+    Rscript ./scripts/analysis.R ../cm_cc/data/{network}/edge_list_iids.tsv   ../cm_cc/output/{network}/stats/{network}_output_itr_{itr}_wcc_stats.tsv   ../cm_cc/output/{network}/{network}_output_itr_{itr}_wcc.tsv
+"""
+
+
+os.system(terminal_command)
