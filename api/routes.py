e37f9b79848b55618f1de73fb809bd7455e2c80c
Joao Alfredo Cardoso Lamy <tntlamy@gmail.com>
2025-02-14 15:22:59 -0300

    cleanup - removal of unecessary code

--- api/routes.py
+++ api/routes.py
@@ -10,25 +10,6 @@ router = APIRouter()
 @router.post("/pipeline", response_model=str, status_code=status.HTTP_201_CREATED, tags=["items"])
 def run_pipeline(algoIn: AlgoIn): 
 
-    # if algoIn.algo_name == "sbm":
-    #     algoIn.generate_cargs_json()
-
-    #     timestamp = "-" + datetime.now().strftime("%Y%m%d-%H:%M:%S")
-    #     folder_name = "example" + timestamp
-
-    #     terminal_command = f"""
-    #     cd ..
-    #     cd cm_pipeline
-    #     python3 -m hm01.cm -i ../api/{algoIn.file_path} -o ../api/samples/{folder_name}/output.tsv -c external  -cargs ../api/cargs.json -cfile /home/tomasa/dashboard-cm/cm_pipeline/hm01/clusterers/external_clusterers/sbm_wrapper.py
-    #     ..
-    #     """
-
-    #     os.system(terminal_command)
-
-    #     return "Success"
-
-
-    # else:
     json_path, input_dir =  algoIn.callJSON()
 
     terminal_command = f"""
@@ -44,43 +25,3 @@ def run_pipeline(algoIn: AlgoIn):
 
     return "Success"
 
-
-
-
-@router.post("/cluster", response_model=str, status_code=status.HTTP_201_CREATED, tags=["items"])
-def run_cluster(algoIn: AlgoIn): 
-
-    if algoIn.algo_name == "sbm":
-        algoIn.generate_cargs_json()
-
-        timestamp = "-" + datetime.now().strftime("%Y%m%d-%H:%M:%S")
-        folder_name = "example" + timestamp
-
-        terminal_command = f"""
-        cd ..
-        cd cm_pipeline
-        python3 -m hm01.cm -i ../api/{algoIn.file_path} -o ../api/samples/{folder_name}/output.tsv -c external  -cargs ../api/cargs.json -cfile /home/tomasa/dashboard-cm/cm_pipeline/hm01/clusterers/external_clusterers/sbm_wrapper.py
-        ..
-        """
-
-        os.system(terminal_command)
-
-        return "Success"
-
-
-    else:
-        json_path, input_dir =  algoIn.callJSON()
-
-        terminal_command = f"""
-        cd ..
-        cd cm_pipeline
-        python -m main ../api/{json_path}.json
-        """
-
-        input_dir += "-" + datetime.now().strftime("%Y%m%d-%H:%M:%S")
-        os.system(terminal_command)
-
-        algoIn.postTreatment(input_dir)
-
-        return "Success"
-   
