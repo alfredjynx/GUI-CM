281a44d98596c609b1ac18c9be9a5b1eefc4f6d0
Joao Alfredo Cardoso Lamy <tntlamy@gmail.com>
2025-04-23 17:16:29 -0300

    Number Input Corrections and File Options Removed, Only Upload

--- interface_prototype/home.py
+++ interface_prototype/home.py
@@ -17,41 +17,54 @@ if 'param' not in st.session_state:
 st.title("CM Interface")
 
 file_path = ""
+st.session_state.param = {}
+clustering = False
 
-file_select = st.sidebar.selectbox(
-    'File Options',
-    ('Relative Path', 'Upload','Default')
+
+uploaded_file = st.sidebar.file_uploader(
+    'Edge List File'
 )
 
-if file_select == "Relative Path":
-    file_path = st.sidebar.text_input(
-        "Enter the Relative Path Here! 👇"
+if uploaded_file is not None:
+
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+
+    date = "-".join(str(datetime.now()).split('.')[0].split(' '))
+
+
+    os.makedirs("../api/data/", exist_ok=True)
+
+    with open(f'../api/data/file_{date}.tsv', 'w') as fd:
+        stringio.seek(0)
+        shutil.copyfileobj(stringio, fd)
+
+    file_path = f'./data/file_{date}.tsv'
+
+
+clustering = st.sidebar.checkbox(
+    "Upload Existing Clustering?", value=False
+)
+
+if clustering:
+
+    cluster_uploaded_file = st.sidebar.file_uploader(
+        'Existing Clustering File'
     )
 
-    if file_path:
-        st.write("You entered: ", file_path)
+    if cluster_uploaded_file is not None:
 
-elif file_select == 'Upload':
-
-    uploaded_file = st.sidebar.file_uploader(
-        'Input File'
-    )
-
-    if uploaded_file is not None:
-
-        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+        stringio = StringIO(cluster_uploaded_file.getvalue().decode("utf-8"))
 
         date = "-".join(str(datetime.now()).split('.')[0].split(' '))
 
-        print(date)
 
-        os.makedirs("../api/data/", exist_ok=True)
+        os.makedirs("../api/clustering/", exist_ok=True)
 
-        with open(f'../api/data/file_{date}.tsv', 'w') as fd:
+        with open(f'../api/clustering/file_{date}.tsv', 'w') as fd:
             stringio.seek(0)
             shutil.copyfileobj(stringio, fd)
 
-        file_path = f'./data/file_{date}.tsv'
+        st.session_state.param["existing_clustering"] = f'clustering/file_{date}.tsv'
 
 
 algorithm = st.sidebar.selectbox(
@@ -60,24 +73,19 @@ algorithm = st.sidebar.selectbox(
 )
 
 if algorithm == 'Leiden-CPM':
-    st.session_state.param = {}
-    resolution = st.sidebar.number_input(label= "Resolution", value= 0.001, format="%f")
-    iteration = st.sidebar.number_input(label= "Iterations", value= 2)
+    resolution = st.sidebar.number_input(label= "Resolution", value= 0.001, format="%f", min_value=0.0, step=0.001)
+    iteration = st.sidebar.number_input(label= "Iterations", min_value=1, max_value=100, step=1, value=1)
     st.session_state.param["res"] = float(resolution)
     st.session_state.param["i"] = int(iteration)
     clustering_algorithm = 'leiden'
 elif algorithm == 'Leiden-Mod':
-    st.session_state.param = {}
     iteration = st.sidebar.number_input(label= "Iterations", min_value=1, max_value=100, step=1, value=1)
     clustering_algorithm = 'leiden_mod'
     if iteration is not None:
-        print(iteration)
         st.session_state.param["i"] = int(iteration)
 elif algorithm == 'Infomap':
-    st.session_state.param = {}
     clustering_algorithm = 'infomap'
 elif algorithm == "Stochastic Block Model (SBM)":
-    st.session_state.param = {}
     
     block_state = st.sidebar.selectbox(
         "Select block state:",
@@ -86,16 +94,18 @@ elif algorithm == "Stochastic Block Model (SBM)":
     
     block_state_dict = {"Non Nested":"non_nested_sbm", "Planted Partition Model": "planted_partition_model"}
 
-    degree_corrected = st.sidebar.checkbox(
-        "Degree corrected", value=False
-    )
+    if block_state == "Non Nested":
+        degree_corrected = st.sidebar.checkbox(
+            "Degree corrected", value=False
+        )
+        st.session_state.param["degree_corrected"] = degree_corrected 
+        
     st.session_state.param["block_state"] = block_state_dict[block_state]
-    st.session_state.param["degree_corrected"] = degree_corrected 
     clustering_algorithm = 'sbm'
 
 post_treatment = st.sidebar.selectbox(
     'Post Clustering Treatment',
-    ('None', 'CM', 'CM-CC', 'CM-WCC')
+    ('None', 'CM', 'CM-CC', 'CM-WCC') if not clustering else ('CM', 'CM-CC', 'CM-WCC')
 )
 
 if post_treatment == "CM-CC":
@@ -109,7 +119,7 @@ else:
 
 
 filter_select = st.sidebar.selectbox(
-    'Filter',
+    'Post Clustering Filter',
     ('ON', 'OFF')
 )
 
@@ -120,7 +130,9 @@ if "df" not in st.session_state:
 if "df_stats" not in st.session_state:
     st.session_state.df_stats = None
 
-if st.button("Run CM Pipeline"):
+if st.button("Run CM Pipeline") and len(file_path) > 0:
+    
+    print(st.session_state.param)
 
     st.session_state.pipeline_complete = False  # Reset state
     
@@ -131,38 +143,46 @@ if st.button("Run CM Pipeline"):
         "post_treatment": st.session_state.post_treatment,
         "filter_select": filter_select == "ON"
     }
-    res = requests.post('http://127.0.0.1:8000/pipeline', data= json.dumps(data))
+    res = requests.post('http://backend:8000/pipeline', data= json.dumps(data))
 
     if res.status_code == 201:
         st.session_state.pipeline_complete = True
+        
+        print(res.json()["path"])
 
-        st.session_state.df = pd.read_csv(res.json()["path"], sep="\t", names=["NodeID", "ClusterID"], header=None)
-        st.session_state.df_stats = pd.read_csv(res.json()["stats"])
+        try:
+            st.session_state.df = pd.read_csv(res.json()["path"], sep="\t", header=None)
+            if res.json()["stats"] != "":
+                st.session_state.df_stats = pd.read_csv(res.json()["stats"])
+        except:
+            st.error("Pipeline executed successfully, but the filtering stage left a blank document")
+            st.session_state.pipeline_complete = False
+            
     else:
         st.error(f"Error: Unable to execute the pipeline. Status Code: {res.status_code}")
         st.write(res.text)  
     
-
     
 if st.session_state.pipeline_complete:
     st.success("Pipeline executed successfully! The resource has been created.")
 
     def convert_df(df):
-        return df.to_csv(index=False).encode("utf-8")
+        return df.to_csv(index=False, sep="\t", header=None).encode("utf-8")
 
     csv = convert_df(st.session_state.df)
-    csv_stats = convert_df(st.session_state.df_stats)
 
     st.download_button(
-        label="Download Clustering data as CSV",
+        label="Download Clustering data as TSV",
         data=csv,
-        file_name=clustering_algorithm + ".csv",
+        file_name=clustering_algorithm + ".tsv",
         mime="text/csv",
     )
 
-    st.download_button(
-        label="Download Stats data as CSV",
-        data=csv_stats,
-        file_name=clustering_algorithm + "_stats.csv",
-        mime="text/csv",
-    )
+    if st.session_state.df_stats is not None and "cc" not in st.session_state.post_treatment:
+        csv_stats = convert_df(st.session_state.df_stats)
+        st.download_button(
+            label="Download Stats data as CSV",
+            data=csv_stats,
+            file_name=clustering_algorithm + "_stats.csv",
+            mime="text/csv",
+        )
