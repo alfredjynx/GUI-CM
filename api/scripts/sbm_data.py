e1795c2728ce9cfbc8106f9cf5205cc7a804c81c
Minhyuk Park <minpark815@gmail.com>
2025-04-29 12:22:35 -0500

    Updates sbm naming scheme

--- api/scripts/sbm_data.py
+++ api/scripts/sbm_data.py
@@ -3,10 +3,10 @@ import os
 def get_file_sbm(directory):
 
     files = [
-        "S3_example_sbm.block_statenon_nested_sbm_degree_correctedFalse_make_cm_ready.R.tsv",
-        "S3_example_sbm.block_stateplanted_partition_model_degree_correctedFalse_make_cm_ready.R.tsv",
-        "S3_example_sbm.block_stateplanted_partition_model_degree_correctedTrue_make_cm_ready.R.tsv",
-        "S3_example_sbm.block_statenon_nested_sbm_degree_correctedTrue_make_cm_ready.R.tsv"
+        "S3_example_sbm.degree_correctedFalse_block_statenon_nested_sbm_make_cm_ready.R.tsv",
+        "S3_example_sbm.degree_correctedTrue_block_stateplanted_partition_model_make_cm_ready.R.tsv",
+        "S3_example_sbm.degree_correctedFalse_block_stateplanted_partition_model_make_cm_ready.R.tsv",
+        "S3_example_sbm.degree_correctedTrue_block_statenon_nested_sbm_make_cm_ready.R.tsv",
     ]
 
     # List files in the directory
